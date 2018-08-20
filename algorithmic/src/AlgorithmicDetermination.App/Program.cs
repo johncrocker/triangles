9aaf398e9ad851aad6e705a25f4ef17706208a8e
John Crocker <j.crocker355@gmail.com>
2018-08-20 13:04:17 +0100

    Added App

--- algorithmic/src/AlgorithmicDetermination.App/Program.cs
+++ algorithmic/src/AlgorithmicDetermination.App/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AlgorithmicDetermination.Lib;
+using AlgorithmicDetermination.Lib.Service;
+using AlgorithmicDetermination.Lib.Validator;
+using System;
 
 namespace AlgorithmicDetermination.App
 {
@@ -6,7 +9,49 @@ namespace AlgorithmicDetermination.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a, b, c = 0;
+            ITriangleValidator validator = new TriangleValidator();
+            ITriangleDeterminator service = new TriangleDeterminatorService(validator);
+
+            Console.WriteLine("Guess the triangle");
+            Console.WriteLine("==================");
+
+            Console.Write("Enter length of Side 1 :");
+            a = GetInt();
+
+            Console.Write("Enter length of Side 2 :");
+            b = GetInt();
+
+            Console.Write("Enter length of Side 3 :");
+            c = GetInt();
+
+            Console.WriteLine();
+
+            if (!validator.IsValid(a, b, c))
+            {
+                Console.WriteLine("Sorry, this is not a valid triangle.");
+            }
+            else
+            {
+                Console.WriteLine($"Your triangle is a {service.Determine(a, b, c)} triangle.");
+            }
+
+            Console.WriteLine("Press a key to exit.");
+            Console.ReadKey();
+
+        }
+
+        public static int GetInt()
+        {
+            string input;
+            int i;
+
+            do
+            {
+                input = Console.ReadLine();
+            } while (int.TryParse(input, out i) == false);
+
+            return i;
         }
     }
 }
